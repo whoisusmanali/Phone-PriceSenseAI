d6d1216ae1969d023458f86d8b78af3dfee52229
whoisusmanali <whoisusmanali@gmail.com>
2024-09-07 17:42:30 -0400

    Updated the urls regarding web pages

--- PhonePriceSenseAI/urls.py
+++ PhonePriceSenseAI/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('PriceSenseAI.urls')),
-    path('predict/', include('PriceSenseAI.urls')),
+    #path('predict/', include('PriceSenseAI.urls')),
 ]
--- PriceSenseAI/urls.py
+++ PriceSenseAI/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.predict_price, name='predict_price'),
+    path('predict/', views.predict_price, name='predict'),
+    path('about/', views.about_us, name='about_us'),
+    path('contact/', views.contact_us, name='contact_us'),
 ]
